7661d952a9efb2100fd556ade1ad304cb10a5bce
Caspar Sylwan <caspar.sylwan@gmail.com>
2018-10-01 23:08:55 +0200

    commit4

--- R/appshiny.R
+++ R/appshiny.R
@@ -19,22 +19,35 @@
 
 #Conecting to an api reading in date and transform it timeseries.
 
-riks_api<- function(){
+riks_api <-function(rate1="",rate2="",rate3="",rate4="",rate5="&g97-EUDP3MNOK=on"){
 
-riks_url<-GET("https://www.riksbank.se/en-gb/statistics/search-interest--exchange-rates/?c=cAverage&f=Day&from=03%2f01%2f1999&g2-SECBREPOEFF=on&g97-EUDP3MEUR=on&g97-EUDP3MJPY=on&g97-EUDP3MUSD=on&s=Dot&to=27%2f09%2f2018&export=csv")
-status_code(riks_url)
-repo_day<-content(riks_url,"text",encoding = "UTF-8")
-repo_day<-read_delim(repo_day,delim =  ";")
-repo_day<-separate(repo_day,"Period",c("d","m","y"),convert = T)
-repo_day<-unite(repo_day,"date",c("y","m","d"),sep="-")
-repo_day<-mutate(repo_day,date = ymd(date))}
+  base<-"https://www.riksbank.se/en-gb/statistics/search-interest--exchange-rates/?c=cAverage&s=Dot"
 
+  from<-"&from=03%2f01%2f1999"
+  today<-Sys.Date()
+  today<-as.character(today)
+  today<-strsplit(today,"-")
+  today<-unlist(today)
+  to<-sprintf("&to=%s%%%s%%%s",today[3],today[2],today[1])
 
+  if(rate1!="") {rate1<- "&g97-EUDP3MUSD=on"}
+  if(rate2!="") {rate2<- "&g97-EUDP3MJPY=on"}
+  if(rate3!="") {rate3<- "&g97-EUDP3MGBP=on"}
+  if(rate4!="") {rate4<- "&g97-EUDP3MEUR=on"}
 
+  adres<-paste0(base,to,rate1,rate2,rate3,rate4,rate5,from,"&export=csv")
 
+  riks_url<-GET(adres)
+  status_code(riks_url)
+  repo_day<-content(riks_url,"text",encoding = "UTF-8")
+  repo_day<-read_delim(repo_day,delim =  ";")
+  repo_day<-separate(repo_day,"Period",c("d","m","y"),convert = T)
+  repo_day<-unite(repo_day,"date",c("y","m","d"),sep="-")
+  repo_day<-mutate(repo_day,date = ymd(date))
 
+  return(repo_day)}
 
-repo_day <-riks_api()
+repo_day <-riks_api("a","a","a")
 
 ui<- fluidPage(titlePanel("Repo"),
                sidebarLayout(
@@ -47,7 +60,7 @@ ui<- fluidPage(titlePanel("Repo"),
                  # Select date range to be plotted
 
                  dateRangeInput("date",strong("Date range"),start = "2000-01-03",end = "2018-09-27",
-                                min= "1999-01-03", max= "2018-09-27")),
+                                min= "1999-01-03", max= "2018-10-02")),
 
                mainPanel(
                  plotOutput(outputId = "lineplot", height = "300px"),
