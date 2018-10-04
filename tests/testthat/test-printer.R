a75f883ede622960b3b1cf4290fca9cf2629a353
Caspar Sylwan <caspar.sylwan@gmail.com>
2018-10-04 11:21:56 +0200

    commit 11 Changed testhat

--- tests/testthat/test-printer.R
+++ tests/testthat/test-printer.R
@@ -1,10 +1,10 @@
 context("Testing that it takes in the right data")
 
-expect_equal(names(riks_api()[1,1]),"date")
-expect_equal(class(riks_api())[1],"tbl_df")
-expect_equal(names(riks_api()[1,2]),"Group")
-expect_equal(names(riks_api()[1,3]),"Series")
-expect_equal(names(riks_api()[1,4]),"Value")
+expect_equal(names(riks_api("a")[1,1]),"date")
+expect_equal(class(riks_api("a"))[1],"tbl_df")
+expect_equal(names(riks_api("a")[1,2]),"Group")
+expect_equal(names(riks_api("a")[1,3]),"Series")
+expect_equal(names(riks_api("a")[1,4]),"Value")
 
 expect_identical(riks_api(rate1="a")[1,]$Value,"4.92")
 expect_identical(riks_api(rate1="a")[1,]$Series,"US 3M")
